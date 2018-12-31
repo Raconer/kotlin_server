155f285d935b027139385d566e0b2924d2364ea4
= <=>
2018-12-31 15:02:15 +0900

    kotlin Test Server _V2

--- src/main/kotlin/com/kotlin/server/controller/GreetingController.kt
+++ src/main/kotlin/com/kotlin/server/controller/GreetingController.kt
@@ -2,9 +2,8 @@ package com.kotlin.server.controller
 
 import com.kotlin.server.database.NoticeMapper
 import com.kotlin.server.model.Greeting
-import com.kotlin.server.model.db.DataBase
-import org.jetbrains.exposed.sql.Table
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
@@ -18,15 +17,16 @@ class GreetingController{
 
     val counter = AtomicLong()
 
-    object Notice :Table()
+    @GetMapping("/")
+    fun main(model: Model):String{
+        model.addAttribute("mainData", "Send Main Test Data")
+        //model[""] = "test"
+        return "main"
+    }
 
     @GetMapping("/greeting")
-
     fun greeting(@RequestParam(value = "name", defaultValue = "World") name: String): Greeting {
-        //noticeData.data
-
         val cnt = noticeData.getNoticeCnt()
-
         return Greeting(counter.incrementAndGet(), "Hello, $name $cnt")
     }
 }
--- src/main/kotlin/com/kotlin/server/database/NoticeMapper.kt
+++ src/main/kotlin/com/kotlin/server/database/NoticeMapper.kt
@@ -1,10 +1,7 @@
 package com.kotlin.server.database
 
 import com.kotlin.server.model.db.DataBase
-import org.jetbrains.exposed.sql.Database
-import org.jetbrains.exposed.sql.StdOutSqlLogger
-import org.jetbrains.exposed.sql.addLogger
-import org.jetbrains.exposed.sql.selectAll
+import org.jetbrains.exposed.sql.*
 import org.jetbrains.exposed.sql.transactions.transaction
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Repository
@@ -15,12 +12,14 @@ class NoticeMapper{
     @Autowired
     lateinit var data:DataBase
 
+    object Notice : Table()
+
     fun getNoticeCnt():Int{
         Database.connect(data.url, data.driver, data.username, data.password)
         var result = 0
         transaction {
             addLogger(StdOutSqlLogger)
-            result = com.kotlin.server.controller.GreetingController.Notice.selectAll().count()
+            result = Notice.selectAll().count()
         }
         return result
     }
