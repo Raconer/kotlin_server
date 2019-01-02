9a55c565dcc33ce0f9c59dc8ec95c027a087d30a
= <=>
2019-01-02 12:55:16 +0900

    kotlin Test Server _V3 (Connect WEB Mustache)

--- src/main/kotlin/com/kotlin/server/Application.kt
+++ src/main/kotlin/com/kotlin/server/Application.kt
@@ -8,4 +8,8 @@ class Application
 
 fun main(args: Array<String>) {
     SpringApplication.run(Application::class.java, *args)
+    // Spring version 2.0.2 에서 사용가능
+    /*runApplication<Application>(*args){
+        setBannerMode(Banner.Mode.OFF)
+    }*/
 }
--- src/main/kotlin/com/kotlin/server/controller/GreetingController.kt
+++ src/main/kotlin/com/kotlin/server/controller/GreetingController.kt
@@ -3,13 +3,14 @@ package com.kotlin.server.controller
 import com.kotlin.server.database.NoticeMapper
 import com.kotlin.server.model.Greeting
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.bind.annotation.RestController
 import java.util.concurrent.atomic.AtomicLong
+import javax.servlet.http.HttpServletRequest
 
-@RestController
+@Controller
 class GreetingController{
 
     @Autowired
@@ -18,9 +19,13 @@ class GreetingController{
     val counter = AtomicLong()
 
     @GetMapping("/")
-    fun main(model: Model):String{
+    fun main(model: Model, request:HttpServletRequest):String{
         model.addAttribute("mainData", "Send Main Test Data")
-        //model[""] = "test"
+        //model[""] = "test" // spring 2.0.2.RELEASE 에서 사용가능
+        //model["title"] = "Blog"
+        System.out.println(request.requestURI)
+        System.out.println(request.requestURL)
+
         return "main"
     }
 
